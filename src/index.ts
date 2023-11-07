baa6bfbe056a5da3acadd528ef1e2aa87ccd41ab
Paul Rooney <paul.r@attractiontickets.com>
2023-11-07 11:16:21 +0000

    index

--- src/index.ts
+++ src/index.ts
@@ -90,5 +90,5 @@ app.post("/cancel", (req: Request, res: Response) => {
 })
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000")
+  console.log("Server is running on port [3000]")
 })
