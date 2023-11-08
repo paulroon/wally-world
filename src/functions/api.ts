00708e826b52f53c9b7c586a6290fbddcdfea7c8
Paul Rooney <paul.r@attractiontickets.com>
2023-11-08 11:49:02 +0000

    Moving Fn dir to dist

--- src/functions/api.ts
+++ src/functions/api.ts
@@ -1,11 +1,11 @@
 import express from "express"
-import routes from "../src/routes"
+import routes from "../routes"
 import serverless from "serverless-http/serverless-http"
 
 const app = express()
 
 app.use(express.json())
 
-app.use(routes)
+app.use("/", routes)
 
 export const handler = serverless(app)
